1ba7d4ad0f0aa48443b47b518aee29a63832f841
keemooc_pc <505927071@qq.com>
2016-05-20 01:38:01 +0800

    add

---
+++ TestGame/Assets/Test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TestMem : Object
+{
+    int i = 0;
+}
+public class Test : MonoBehaviour
+{
+
+    public void Update()
+    {
+        TestMem _list = new TestMem();
+        Debug.Log(_list);
+        Debug.Log(Profiler.GetRuntimeMemorySize(new GameObject()));
+    }
+}
